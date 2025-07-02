1e14f12dbb204063c90c38eb850d4af402fa0551
YunKaga <voronzowyura@gmail.com>
2025-07-02 16:04:32 +0700

    Удален каталог build/ для самостоятельной компиляции

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Sudoku_FILE /home/voron/prog/Sudoku-project/build/Sudoku)
-set(__QT_DEPLOY_TARGET_Sudoku_TYPE EXECUTABLE)
--- build/CMakeFiles/Sudoku.dir/Sudoku_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Sudoku.dir/Sudoku_autogen/mocs_compilation.cpp.o: \
- /home/voron/prog/Sudoku-project/build/Sudoku_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
